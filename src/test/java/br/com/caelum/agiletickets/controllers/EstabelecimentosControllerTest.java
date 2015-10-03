ef773ae1798387599ada838ecbb1c5f74ab803f5
Marcelo Massuda <agil5234@caelum131-04>
2015-10-03 15:31:06 -0300

    teste com mockito

--- src/test/java/br/com/caelum/agiletickets/controllers/EstabelecimentosControllerTest.java
+++ src/test/java/br/com/caelum/agiletickets/controllers/EstabelecimentosControllerTest.java
@@ -1,8 +1,16 @@
 package br.com.caelum.agiletickets.controllers;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
@@ -13,8 +21,6 @@ import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.caelum.vraptor.util.test.MockValidator;
 import br.com.caelum.vraptor.validator.ValidationException;
 import br.com.caelum.vraptor.validator.Validator;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class EstabelecimentosControllerTest {
 
@@ -62,4 +68,17 @@ public class EstabelecimentosControllerTest {
 		verify(diretorio).adiciona(estabelecimento);
 	}
 	
+	@Test
+	public void deveListarTodosDoDiretorio() {
+		DiretorioDeEstabelecimentos DAO = Mockito.mock(DiretorioDeEstabelecimentos.class);
+		List<Estabelecimento> emptyList = Collections.emptyList();
+		Mockito.when(DAO.todos()).thenReturn(emptyList);
+		
+		EstabelecimentosController sujeito = new EstabelecimentosController(null, null, DAO);
+		
+		List<Estabelecimento> todos = sujeito.lista();
+		
+		assertEquals(emptyList, todos);
+		
+	}
 }
